c8ef54bb96eee944df46c88a569a1db242f3f3a4
Matija Brnas <mb8828@rit.edu>
2023-11-30 01:13:24 +0100

    Initial commit

---
+++ src/test/java/com/company/blogplatform/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.blogplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
