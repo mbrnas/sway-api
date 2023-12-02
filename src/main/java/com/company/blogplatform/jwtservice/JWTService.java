755e0209a57adc795aeb927d8de1ce8a580dd3c3
Matija Brnas <mb8828@rit.edu>
2023-12-02 23:04:14 +0100

    role enum, user properties, pomxml jwt dependencies

---
+++ src/main/java/com/company/blogplatform/jwtservice/JWTService.java
@@ -0,0 +1,4 @@
+package com.company.blogplatform.jwtservice;
+
+public interface JWTService {
+}
---
+++ src/main/java/com/company/blogplatform/jwtservice/impl/JWTServiceImpl.java
@@ -0,0 +1,43 @@
+package com.company.blogplatform.jwtservice.impl;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTServiceImpl {
+    private String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Key getSigninKey() {
+        byte[] key = Decoders.BASE64.decode("413F4428472B4B6250655368566D5970337336763979244226452948404D6351");
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigninKey()).build().parseClaimsJws(token).getBody();
+    }
+
+}
