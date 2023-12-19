89f57f53b8b6ce6c34c221fb0cd8bf30797235db
Matija Brnas <mb8828@rit.edu>
2023-12-19 09:42:53 +0100

    test maven.yml

--- src/main/java/com/company/blogplatform/controller/post/PostController.java
+++ src/main/java/com/company/blogplatform/controller/post/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//test
 @RestController
 @RequestMapping("/api/v1/user")
 @CrossOrigin(origins = "http://localhost:3000")
